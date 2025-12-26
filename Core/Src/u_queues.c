a1d0085f38d45e823aacbabd34dd58a9c75a6728
Daniel Nakhooda <38150780+dnakhooda@users.noreply.github.com>
2025-12-26 00:13:58 -0500

    Fixes (#25)

--- Core/Src/u_queues.c
+++ Core/Src/u_queues.c
@@ -3,17 +3,6 @@
 #include "u_tx_debug.h"
 #include <stdio.h>
 
-/* 
-*  NOTE: This file is kinda weird because of how ThreadX queues work. The size of each message in a ThreadX queue has to be a multiple of 4 bytes, since ThreadX
-*  queues are implemented as arrays of 32-bit words. So, to create queues for structs that aren't a multiple of 4 bytes, you have to round up to the nearest multiple of 4 bytes.
-*  This is handled in the _create_queue() function below.
-*
-*  Then, the queue_send() and queue_receive() functions use a buffer to send/receive messages to/from the queue.
-*  This handles any conversions between the actual message size and the rounded-up size.
-*
-*  On the bright side (assuming my code works), this file should automatically handle all the 32-bit word stuff for you so you don't have to worry about it.
-*/
-
 /* Incoming CAN Queue */
 queue_t can_incoming = {
     .name = "Incoming CAN Queue",           /* Name of the queue. */
